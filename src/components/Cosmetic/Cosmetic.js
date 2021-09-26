f8fc9ebe583c5f62d46bd76bb63a7fa2975636b2
shamim <shamim.pstu@gmail.com>
2021-09-26 08:38:57 +0600

    update array reducer

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { totalPrice } from '../../utilities/habijabi';
-import { addDB } from '../../utilities/localDB';
+import { totalPrice } from '../../utilities/arrayReduce';
+import { addDB , removeDB } from '../../utilities/localDB';
 
 const Cosmetic = (props) => {
     const {id , name ,price} = props.cosmetic
 
 
 
-    const handlePurchase = (id) => {
+    const handleAddDB = (id) => {
         addDB(id);
     }
+
+    const handleRemoveDB = (id) => {
+        removeDB(id);
+    }
+
+
+
     return (
         <div>
             <h2>name:{name}</h2>
             <h2>{id}<small> price:{price}</small></h2>
-            <button onClick = {()=>handlePurchase(id)}>Purchase</button>
+            <h2>total Price using reducer is : {totalPrice}</h2>
+            <button onClick = {()=>handleAddDB(id)}>Add to Cart</button>
+            <button onClick = {()=>handleRemoveDB(id)}>Remove from Cart</button>
         </div>
     );
 };
--- src/utilities/localDB.js
+++ src/utilities/localDB.js
@@ -1,14 +1,62 @@
-const addDB = id => {
-    const exists = localStorage.getItem(id)
-    console.log(exists);
-   if(!exists){
-    localStorage.setItem(id , 1);
-   }
-    else{
 
-        localStorage.setItem(id , parseInt(exists)+1)
+// add purchased item to local storage as a single item 
+// const addDB = id => {
+//     const exists = localStorage.getItem(id)
+
+//    if(!exists){
+//     localStorage.setItem(id , 1);
+//    }
+//     else{
+
+//         localStorage.setItem(id , parseInt(exists)+1)
+//     }
+// }
+
+
+// add purchased item to local storage as a object
+
+const addDB = id => {
+
+    const exist = localStorage.getItem('cart');
+    let cart = {};
+    if (!exist) {
+        cart[id] = 1;
     }
+    else {
+
+        cart = JSON.parse(exist)
+        if (!cart[id]) {
+            cart[id] = 1
+        }
+        else {
+            cart[id] = cart[id] + 1
+        }
+
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+const removeDB = (id) => {
+
+    const exist = localStorage.getItem('cart');
+
+    if(!exist){
+
+    }
+
+    else {
+        const cart = JSON.parse(exist)
+        delete cart[id] ; 
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }
+
+    
 }
 
 
-export {addDB} 
+
+
+
+
+export { addDB , removeDB }
